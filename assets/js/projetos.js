27886d124b5676b79136456267d91d0bb7056f23
Luan Gabriel <luan.gabriel.alves@outlook.com>
2025-09-04 13:58:23 -0300

    att06092025

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -6,7 +6,7 @@ function projetos(section) {
 
   <div class="flex items-center justify-center min-h-screen">
     <!-- Container fixo com rolagem -->
-    <div class="w-full max-w-5xl h-[80vh] overflow-y-auto bg-white rounded-lg shadow-lg p-6 space-y-6">
+    <div class="w-full max-w-5xl h-[80vh] overflow-y-auto bg-white dark:bg-gray-900 rounded-lg shadow-lg p-6 space-y-6 text-gray-900 dark:text-white">
 
       <!-- CARD 1 -->
       <div class="card p-4 bg-gray-50 rounded-lg shadow-sm">
@@ -28,7 +28,12 @@ function projetos(section) {
             </a>
           </div>
         </div>
-        <section class="projeto">
+
+
+
+
+
+        <section class="projeto" id="projeto-textos">
           <h2>📄 Projeto: Empregos</h2>
           <p><strong>Descrição:</strong><br>
           Aplicação web desenvolvida para facilitar a divulgação de vagas de emprego na cidade de Franca-SP. O projeto tem como foco a acessibilidade e simplicidade, permitindo que usuários encontrem oportunidades locais de forma rápida e intuitiva.</p>
@@ -80,7 +85,7 @@ function projetos(section) {
             </a>
           </div>
         </div>
-        <section class="projeto">
+        <section class="projeto" id="projeto-textos">
           <h2>👕 Projeto: Camisas</h2>
           <p><strong>Descrição:</strong><br>
           Aplicação web interativa que permite aos usuários personalizar camisetas em um ambiente 3D. Utiliza renderização avançada com Three.js para criar visualizações realistas e dinâmicas dos modelos.</p>
@@ -115,28 +120,32 @@ function projetos(section) {
 
       <!-- CARD 3 -->
       <div class="card p-4 bg-gray-50 rounded-lg shadow-sm">
-        <h3 class="text-xl font-bold mb-2">LGATEC</h3>
-        <div class="img-wrapper relative mb-2">
-          <video width="100%" height="auto" controls poster="./assets/images/projects/thumblgatec.png" class="rounded-md mb-2">
-            <source src="./assets/images/projects/videolgatec.mp4" type="video/mp4">
-            Seu navegador não suporta vídeo.
-          </video>
-          <a href="https://github.com/luan-gabriel/lgatec?tab=readme-ov-file#lgatec" target="_blank" title="Repositório no Github">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path fill="none" d="M0 0h24v24H0z" />
-                  <path
-                    d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z"
-                    fill="rgba(255,255,255,1)"
-                  />
-                </svg>
-              </a>
+  <h3 class="text-xl font-bold mb-2">LGATEC</h3>
+  <div class="img-wrapper relative mb-2">
+    <video width="100%" height="auto" controls poster="./assets/images/projects/thumblgatec.png" class="rounded-md mb-2">
+      <source src="./assets/images/projects/videolgatec.mp4" type="video/mp4">
+      Seu navegador não suporta vídeo.
+    </video>
+    
+    <div class="flex items-center space-x-3">
+      <!-- Link GitHub -->
+      <a href="https://github.com/luan-gabriel/lgatec?tab=readme-ov-file#lgatec" target="_blank" title="Repositório no Github">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z" fill="rgba(255,255,255,1)"/>
+        </svg>
+      </a>
+
+      <!-- Botão para o site -->
+         <a href="http://146.235.25.36:8069?db=odoo17_banco" target="_blank" title="Acessar Site" id="site-btn">
+        Acessar Site
+      </a>
+    </div>
+    </div>
+        
         </div>
-        <section class="projeto">
+        
+        <section class="projeto" id="projeto-textos">
           <h2>🧾 Projeto: Lgatec (Odoo)</h2>
           <p><strong>Descrição:</strong><br>
           Este projeto consiste em uma instalação e configuração personalizada do ERP <strong>Odoo</strong>, utilizada no servidor da empresa <strong>Lgatec</strong>. O repositório reúne módulos customizados, ajustes técnicos e scripts para facilitar o desenvolvimento e o deploy da plataforma.</p>
